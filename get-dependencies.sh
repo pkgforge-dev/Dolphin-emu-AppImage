964e779b999b5343fb18be9b3cf0299fa34272b8
Samuel <36420837+Samueru-sama@users.noreply.github.com>
2025-06-16 10:12:18 -0400

    add `xcb-util-cursor`, switch to `gcc14`

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -51,6 +51,7 @@ pacman -Syu --noconfirm \
 	vulkan-nouveau \
 	vulkan-radeon \
 	wget \
+	xcb-util-cursor \
 	xorg-server-xvfb \
 	xxhash \
 	xz \
@@ -58,7 +59,7 @@ pacman -Syu --noconfirm \
 	zsync
 
 if [ "$(uname -m)" = 'x86_64' ]; then
-	pacman -Syu --noconfirm vulkan-intel haskell-gnutls gcc13 svt-av1
+	pacman -Syu --noconfirm vulkan-intel haskell-gnutls gcc14 svt-av1
 else
 	pacman -Syu --noconfirm vulkan-freedreno vulkan-panfrost vulkan-broadcom
 fi
