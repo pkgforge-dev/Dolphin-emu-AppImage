c259bcf9f6c51f0454ce7537da2d247858142ef1
Samuel <36420837+Samueru-sama@users.noreply.github.com>
2025-08-23 07:34:03 -0400

    simplify packages install

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -1,79 +1,47 @@
 #!/bin/sh
 
 set -ex
-
-sed -i 's/DownloadUser/#DownloadUser/g' /etc/pacman.conf
-
-if [ "$(uname -m)" = 'x86_64' ]; then
-	PKG_TYPE='x86_64.pkg.tar.zst'
-else
-	PKG_TYPE='aarch64.pkg.tar.xz'
-fi
-
-LLVM_URL="https://github.com/pkgforge-dev/llvm-libs-debloated/releases/download/continuous/llvm-libs-mini-$PKG_TYPE"
-FFMPEG_URL="https://github.com/pkgforge-dev/llvm-libs-debloated/releases/download/continuous/ffmpeg-mini-$PKG_TYPE"
-QT6_URL="https://github.com/pkgforge-dev/llvm-libs-debloated/releases/download/continuous/qt6-base-iculess-$PKG_TYPE"
-LIBXML_URL="https://github.com/pkgforge-dev/llvm-libs-debloated/releases/download/continuous/libxml2-iculess-$PKG_TYPE"
-OPUS_URL="https://github.com/pkgforge-dev/llvm-libs-debloated/releases/download/continuous/opus-nano-$PKG_TYPE"
-MESA_URL="https://github.com/pkgforge-dev/llvm-libs-debloated/releases/download/continuous/mesa-mini-$PKG_TYPE"
+EXTRA_PACKAGES="https://raw.githubusercontent.com/pkgforge-dev/Anylinux-AppImages/refs/heads/main/useful-tools/get-debloated-pkgs.sh"
 
 echo "Installing build dependencies..."
 echo "---------------------------------------------------------------"
 pacman -Syu --noconfirm \
-	alsa-lib \
-	base-devel \
-	bluez-libs \
-	bzip2 \
-	cmake \
-	curl \
-	enet \
-	gcc-libs \
-	git \
-	glibc \
-	hicolor-icon-theme \
-	hidapi \
-	libusb \
-	libx11 \
-	libxi \
-	libxrandr \
-	lz4 \
-	lzo \
-	mesa \
-	patchelf \
-	pipewire-audio \
-	pulseaudio \
-	pulseaudio-alsa \
-	qt6ct \
-	sdl2 \
-	speexdsp \
-	strace \
-	vulkan-headers \
-	vulkan-nouveau \
-	vulkan-radeon \
-	wget \
-	xcb-util-cursor \
+	base-devel       \
+	bluez-libs       \
+	bzip2            \
+	cmake            \
+	curl             \
+	enet             \
+	git              \
+	hidapi           \
+	libusb           \
+	libx11           \
+	libxi            \
+	libxrandr        \
+	lz4              \
+	lzo              \
+	mesa             \
+	patchelf         \
+	pipewire-audio   \
+	pulseaudio       \
+	pulseaudio-alsa  \
+	qt6ct            \
+	sdl2             \
+	speexdsp         \
+	strace           \
+	vulkan-headers   \
+	vulkan-nouveau   \
+	vulkan-radeon    \
+	wget             \
+	xcb-util-cursor  \
 	xorg-server-xvfb \
-	xxhash \
-	xz \
-	zstd \
+	xxhash           \
+	xz               \
+	zstd             \
 	zsync
 
-if [ "$(uname -m)" = 'x86_64' ]; then
-	pacman -Syu --noconfirm vulkan-intel haskell-gnutls gcc14 svt-av1
-else
-	pacman -Syu --noconfirm vulkan-freedreno vulkan-panfrost vulkan-broadcom
-fi
-
-echo "Installing debloated pckages..."
+echo "Installing debloated packages..."
 echo "---------------------------------------------------------------"
-wget --retry-connrefused --tries=30 "$LLVM_URL"   -O  ./llvm-libs.pkg.tar.zst
-wget --retry-connrefused --tries=30 "$QT6_URL"    -O  ./qt6-base-iculess.pkg.tar.zst
-wget --retry-connrefused --tries=30 "$LIBXML_URL" -O  ./libxml2-iculess.pkg.tar.zst
-wget --retry-connrefused --tries=30 "$FFMPEG_URL" -O  ./ffmpeg-mini.pkg.tar.zst
-wget --retry-connrefused --tries=30 "$OPUS_URL"   -O  ./opus-nano.pkg.tar.zst
-
-pacman -U --noconfirm ./*.pkg.tar.zst
-rm -f ./*.pkg.tar.zst
-
-echo "All done!"
-echo "---------------------------------------------------------------"
+wget --retry-connrefused --tries=30 "$EXTRA_PACKAGES" -O ./get-debloated-pkgs.sh
+chmod +x ./get-debloated-pkgs.sh
+./get-debloated-pkgs.sh --add-common
