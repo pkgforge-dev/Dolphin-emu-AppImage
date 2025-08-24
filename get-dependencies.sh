9086ecbc4963e2865b8e94156d2fe60b861e266f
Samuel <36420837+Samueru-sama@users.noreply.github.com>
2025-08-24 19:43:32 -0400

    test

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -21,7 +21,6 @@ pacman -Syu --noconfirm \
 	lz4              \
 	lzo              \
 	mesa             \
-	patchelf         \
 	pipewire-audio   \
 	pulseaudio       \
 	pulseaudio-alsa  \
@@ -30,8 +29,6 @@ pacman -Syu --noconfirm \
 	speexdsp         \
 	strace           \
 	vulkan-headers   \
-	vulkan-nouveau   \
-	vulkan-radeon    \
 	wget             \
 	xcb-util-cursor  \
 	xorg-server-xvfb \
@@ -45,3 +42,33 @@ echo "---------------------------------------------------------------"
 wget --retry-connrefused --tries=30 "$EXTRA_PACKAGES" -O ./get-debloated-pkgs.sh
 chmod +x ./get-debloated-pkgs.sh
 ./get-debloated-pkgs.sh --add-common
+
+echo "Building dolphin..."
+echo "---------------------------------------------------------------"
+REPO="https://github.com/dolphin-emu/dolphin.git"
+GRON="https://raw.githubusercontent.com/xonixx/gron.awk/refs/heads/main/gron.awk"
+
+# Determine to build nightly or stable
+if [ "$DEVEL" = 'true' ]; then
+	echo "Making nightly build of dolphin-emu..."
+	VERSION="$(git ls-remote "$REPO" HEAD | cut -c 1-9 | head -1)"
+	UPINFO="$(echo "$UPINFO" | sed 's|latest|nightly|')"
+	git clone "$REPO" ./dolphin
+else
+	echo "Making stable build of dolphin-emu..."
+	wget "$GRON" -O ./gron.awk
+	chmod +x ./gron.awk
+	VERSION=$(wget https://api.github.com/repos/dolphin-emu/dolphin/tags -O - | \
+		./gron.awk | grep -v "nJoy" | awk -F'=|"' '/name/ {print $3}' | \
+		sort -V -r | head -1)
+	git clone --branch "$VERSION" --single-branch "$REPO" ./dolphin
+fi
+
+# BUILD DOLPHIN
+cd ./dolphin 
+mkdir ./build 
+cd ./build
+git submodule update --init --recursive
+cmake .. -DLINUX_LOCAL_DEV=true -DCMAKE_POLICY_VERSION_MINIMUM=3.5
+make -j $(nproc)
+sudo make install
