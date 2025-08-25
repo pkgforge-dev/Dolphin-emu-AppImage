d7c554f7fc204d0ef53e1ddceb77c031a0d953ad
Samuel <36420837+Samueru-sama@users.noreply.github.com>
2025-08-25 20:18:44 +0000

    simplify CI and dolphin build process (#33)
    * test
    
    * test
    
    * Update dolphin-emu-appimage.sh
    
    * Update get-dependencies.sh
    
    * Update get-dependencies.sh
    
    * Update dolphin-emu-appimage.sh
    
    * Update get-dependencies.sh
    
    * Update get-dependencies.sh
    
    * Update get-dependencies.sh
    
    * Update dolphin-emu-appimage.sh
    
    * Update stable.yml
    
    * Update nightly.yml

--- dolphin-emu-appimage.sh
+++ dolphin-emu-appimage.sh
@@ -2,44 +2,11 @@
 
 set -ex
 
-REPO="https://github.com/dolphin-emu/dolphin.git"
-GRON="https://raw.githubusercontent.com/xonixx/gron.awk/refs/heads/main/gron.awk"
 export APPIMAGE_EXTRACT_AND_RUN=1
 export ARCH="$(uname -m)"
 
-# Determine to build nightly or stable
-if [ "$DEVEL" = 'true' ]; then
-	echo "Making nightly build of dolphin-emu..."
-	VERSION="$(git ls-remote "$REPO" HEAD | cut -c 1-9 | head -1)"
-	UPINFO="$(echo "$UPINFO" | sed 's|latest|nightly|')"
-	git clone "$REPO" ./dolphin
-else
-	echo "Making stable build of dolphin-emu..."
-	wget "$GRON" -O ./gron.awk
-	chmod +x ./gron.awk
-	VERSION=$(wget https://api.github.com/repos/dolphin-emu/dolphin/tags -O - | \
-		./gron.awk | grep -v "nJoy" | awk -F'=|"' '/name/ {print $3}' | \
-		sort -V -r | head -1)
-	git clone --branch "$VERSION" --single-branch "$REPO" ./dolphin
-fi
-
-# BUILD DOLPHIN
-(
-	cd ./dolphin 
-	mkdir ./build 
-	cd ./build
-
-	git submodule update --init --recursive
-	cmake .. -DLINUX_LOCAL_DEV=true -DCMAKE_POLICY_VERSION_MINIMUM=3.5
-	make -j $(nproc)
-	sudo make install
-
-	sudo cp -r ../Data/Sys /usr/local/bin
-	sudo cp -r ./Source/Core/DolphinQt /usr/local/bin
-  )
-
 # Deploy AppImage
-[ -n "$VERSION" ] && echo "$VERSION" > ~/version
+VERSION="$(cat ~/version)"
 APPIMAGETOOL="https://github.com/pkgforge-dev/appimagetool-uruntime/releases/download/continuous/appimagetool-$ARCH.AppImage"
 URUNTIME="https://raw.githubusercontent.com/pkgforge-dev/Anylinux-AppImages/refs/heads/main/useful-tools/uruntime2appimage.sh"
 SHARUN="https://raw.githubusercontent.com/pkgforge-dev/Anylinux-AppImages/refs/heads/main/useful-tools/quick-sharun.sh"
@@ -47,6 +14,8 @@ SHARUN="https://raw.githubusercontent.com/pkgforge-dev/Anylinux-AppImages/refs/h
 export ADD_HOOKS="self-updater.bg.hook"
 export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}|latest|*dwarfs-$ARCH.AppImage.zsync"
 export OUTNAME=Dolphin_Emulator-"$VERSION"-anylinux.dwarfs-"$ARCH".AppImage
+export PATH_MAPPING_RELATIVE=1 # Dolphin is hardcoded to /urs/share
+export DEPLOY_LOCALE=1
 export DEPLOY_OPENGL=1 
 export DEPLOY_VULKAN=1 
 export DEPLOY_PIPEWIRE=1
@@ -54,15 +23,7 @@ export DEPLOY_PIPEWIRE=1
 # Bundle all libs
 wget --retry-connrefused --tries=30 "$SHARUN" -O ./quick-sharun
 chmod +x ./quick-sharun
-./quick-sharun /usr/local/bin/dolphin-*
-
-# copy locales, the dolphin binary expects them here
-mkdir -p ./AppDir/Source/Core
-cp -r /usr/local/bin/DolphinQt ./AppDir/Source/Core
-find ./AppDir/Source/Core/DolphinQt -type f ! -name 'dolphin-emu.mo' -delete
-
-# when compiled portable this directory needs a capital S
-cp -rv /usr/local/bin/Sys ./AppDir/bin/Sys
+./quick-sharun /usr/bin/dolphin-*
 
 # Force C locale due to issues with gconv causing crashes
 # See https://github.com/pkgforge-dev/Dolphin-emu-AppImage/issues/28
@@ -91,4 +52,7 @@ wget --retry-connrefused --tries=30 "$APPIMAGETOOL" -O ./appimagetool
 chmod +x ./appimagetool
 ./appimagetool -n -u "$UPINFO" "$PWD"/AppDir "$PWD"/Dolphin_Emulator-"$VERSION"-anylinux.squashfs-"$ARCH".AppImage
 
+mkdir -p ./dist
+mv -v ./*.AppImage*  ./dist
+mv -v ~/version      ./dist
 echo "All Done!"
--- get-dependencies.sh
+++ get-dependencies.sh
@@ -21,17 +21,13 @@ pacman -Syu --noconfirm \
 	lz4              \
 	lzo              \
 	mesa             \
-	patchelf         \
 	pipewire-audio   \
 	pulseaudio       \
 	pulseaudio-alsa  \
 	qt6ct            \
 	sdl2             \
 	speexdsp         \
-	strace           \
 	vulkan-headers   \
-	vulkan-nouveau   \
-	vulkan-radeon    \
 	wget             \
 	xcb-util-cursor  \
 	xorg-server-xvfb \
@@ -44,4 +40,44 @@ echo "Installing debloated packages..."
 echo "---------------------------------------------------------------"
 wget --retry-connrefused --tries=30 "$EXTRA_PACKAGES" -O ./get-debloated-pkgs.sh
 chmod +x ./get-debloated-pkgs.sh
-./get-debloated-pkgs.sh --add-common
+sed -i 's|set -e|set -ex|' ./get-debloated-pkgs.sh
+./get-debloated-pkgs.sh --add-mesa llvm-libs-nano libxml2-mini qt6-base-mini gtk3-mini opus-mini
+
+echo "Building dolphin..."
+echo "---------------------------------------------------------------"
+REPO="https://github.com/dolphin-emu/dolphin.git"
+GRON="https://raw.githubusercontent.com/xonixx/gron.awk/refs/heads/main/gron.awk"
+
+# Determine to build nightly or stable
+if [ "$DEVEL" = 'true' ]; then
+	echo "Making nightly build of dolphin-emu..."
+	VERSION="$(git ls-remote "$REPO" HEAD | cut -c 1-9 | head -1)"
+	UPINFO="$(echo "$UPINFO" | sed 's|latest|nightly|')"
+	git clone "$REPO" ./dolphin
+else
+	echo "Making stable build of dolphin-emu..."
+	wget "$GRON" -O ./gron.awk
+	chmod +x ./gron.awk
+	VERSION=$(wget https://api.github.com/repos/dolphin-emu/dolphin/tags -O - | \
+		./gron.awk | grep -v "nJoy" | awk -F'=|"' '/name/ {print $3}' | \
+		sort -V -r | head -1)
+	git clone --branch "$VERSION" --single-branch "$REPO" ./dolphin
+fi
+echo "$VERSION" > ~/version
+
+# BUILD DOLPHIN
+cd ./dolphin 
+mkdir ./build 
+cd ./build
+git submodule update --init --recursive
+cmake .. \
+	-DDISTRIBUTOR=pkgforge-dev   \
+	-DCMAKE_INSTALL_PREFIX=/usr  \
+    -DENABLE_ANALYTICS=OFF       \
+	-DENABLE_LLVM=OFF            \
+	-DUSE_DISCORD_PRESENCE=OFF   \
+    -DENABLE_AUTOUPDATE=OFF      \
+	-DENCODE_FRAMEDUMPS=OFF      \
+	-DCMAKE_POLICY_VERSION_MINIMUM=3.5
+make -j $(nproc)
+sudo make install
