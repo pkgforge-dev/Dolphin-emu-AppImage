7ae2bb3432a2d720bf921bcae6472e175137e88c
Samuel <36420837+Samueru-sama@users.noreply.github.com>
2025-12-09 16:44:33 -0400

    remove gtk from CI runner (#43)

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -42,7 +42,7 @@ echo "Installing debloated packages..."
 echo "---------------------------------------------------------------"
 wget --retry-connrefused --tries=30 "$EXTRA_PACKAGES" -O ./get-debloated-pkgs.sh
 chmod +x ./get-debloated-pkgs.sh
-./get-debloated-pkgs.sh --add-mesa llvm-libs-nano libxml2-mini qt6-base-mini gtk3-mini opus-mini gdk-pixbuf2-mini librsvg-mini
+./get-debloated-pkgs.sh --add-mesa llvm-libs-nano libxml2-mini qt6-base-mini opus-mini
 
 echo "Building dolphin..."
 echo "---------------------------------------------------------------"
